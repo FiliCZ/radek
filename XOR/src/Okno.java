7b523d28417d2a18d483351498be5f84e3adeceb
skrivanek <skrivanek@PC536.SPSEJECNANET.CZ>
2018-03-20 11:11:36 +0100

    XOR

--- XOR/src/Okno.java
+++ XOR/src/Okno.java
@@ -1,9 +1,18 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class Okno extends JFrame {
-	private JLabel label;
-	private JTextField field;
-
+	private JLabel label1;
+	private JTextField field1;
+	private JLabel label2;
+	private JTextField field2;
+    private JButton button;
+    
+public Okno() {
+	this.setTitle("XOR");
+	this.setSize(200, 200);
+}
+    
 }
