3df74899c77e733dbc627f8701138ba80f2c0c7b
simek2 <simek2@PC537.SPSEJECNANET.CZ>
2018-03-20 11:19:30 +0100

    ODRADKOVÁNÍ

--- XOR/src/Okno.java
+++ XOR/src/Okno.java
@@ -12,17 +12,17 @@ public class Okno extends JFrame {
 	private JTextField field2;
     private JButton button;
     
-public Okno() {
-	this.setTitle("XOR");
-	this.setSize(200, 200);
-	this.setLayout(new FlowLayout());
-	label1 = new JLabel("Cesta k souboru");
-	this.add(label1);
-	field1 = new JTextField(100);
-	this.add(field1);
-	label2 = new JLabel("Heslo");
-	this.add(label2);
-	field2 = new JTextField(30);
-	this.add(field2);
-}
+	public Okno() {
+		this.setTitle("XOR");
+		this.setSize(200, 200);
+		this.setLayout(new FlowLayout());
+		label1 = new JLabel("Cesta k souboru");
+		this.add(label1);
+		field1 = new JTextField(100);
+		this.add(field1);
+		label2 = new JLabel("Heslo");
+		this.add(label2);
+		field2 = new JTextField(30);
+		this.add(field2);
+	}
 }
