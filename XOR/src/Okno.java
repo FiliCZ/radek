c57beac6039941d4d1b2fb9d166bd880d5b86b0f
skrivanek <skrivanek@PC536.SPSEJECNANET.CZ>
2018-03-20 11:08:04 +0100

    XOR

--- XOR/src/Okno.java
+++ XOR/src/Okno.java
@@ -1,4 +1,9 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
-public class Okno {
+public class Okno extends JFrame {
+	private JLabel label;
+	private JTextField field;
 
 }
